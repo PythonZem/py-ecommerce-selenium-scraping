4af0a997e95fc94f18a33fc70d3d4ec18d8f1462
PythonZem <114096673+PythonZem@users.noreply.github.com>
2024-04-25 20:48:45 +0300

    Update parse.py

--- app/parse.py
+++ app/parse.py
@@ -1,9 +1,21 @@
+import csv
+import dataclasses
 from dataclasses import dataclass
 from urllib.parse import urljoin
 
 
 BASE_URL = "https://webscraper.io/"
+
+ENDPOINTS_URL = {
+    "home": ""
+}
+
 HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
+COMPUTERS_URL = urljoin(BASE_URL, "/test-sites/e-commerce/more/computers/")
+LAPTOPS_URL = urljoin(BASE_URL, "/test-sites/e-commerce/more/computers/laptops")
+TABLETS_URL = urljoin(BASE_URL, "/test-sites/e-commerce/more/computers/tablets")
+PHONES_URL = urljoin(BASE_URL, "/test-sites/e-commerce/more/phones")
+TOUCH_URL = urljoin(BASE_URL, "/test-sites/e-commerce/more/phones/touch")
 
 
 @dataclass
@@ -15,8 +27,18 @@ class Product:
     num_of_reviews: int
 
 
+PRODUCT_FIELDS = [filed.name for filed in dataclasses.fields(Product)]
+
+
 def get_all_products() -> None:
     pass
+
+
+def write_products_to_csv(products: [Product]) -> None:
+    with open("products.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(PRODUCT_FIELDS)
+        writer.writerows([dataclasses.astuple(product) for product in products])
 
 
 if __name__ == "__main__":
